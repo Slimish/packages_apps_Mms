2f0c5db0401a8bb421d35e2b8835b14dd536d1b1
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-03-14 20:32:31 +0700

    [2/4] Mms: Smart Phone Call
    Put on your ear, and this will automatically call current number in message or dialer
    
    Change-Id: Id53ab341c4eb24b6757f418a4516a4f3e87bc7f1
    
    Patchset: 3
    https://gerrit.omnirom.org/#/c/6307/
    Signed-off-by: anerikg <anerikgames@gmail.com>
    
    Conflicts:
    	src/com/android/mms/ui/ComposeMessageActivity.java
    	src/com/android/mms/ui/MessagingPreferenceActivity.java

--- src/com/android/mms/ui/ComposeMessageActivity.java
+++ src/com/android/mms/ui/ComposeMessageActivity.java
@@ -32,6 +32,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.LoaderManager;
 import android.app.ProgressDialog;
+import android.app.Service;
 import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
 import android.content.ClipData;
@@ -155,6 +156,7 @@ import com.android.mms.ui.MessageListView.OnSizeChangedListener;
 import com.android.mms.ui.MessageUtils.ResizeImageResultCallback;
 import com.android.mms.ui.MessagingPreferenceActivity;
 import com.android.mms.ui.RecipientsEditor.RecipientContextMenuInfo;
+import com.android.mms.util.MultiSensorManager;
 import com.android.mms.util.DraftCache;
 import com.android.mms.util.EmojiParser;
 import com.android.mms.util.PhoneNumberFormatter;
@@ -202,7 +204,7 @@ import java.util.regex.Pattern;
  * exit_on_sent boolean Exit this activity after the message is sent.
  */
 public class ComposeMessageActivity extends Activity
-        implements View.OnClickListener, TextView.OnEditorActionListener,
+        implements View.OnClickListener, TextView.OnEditorActionListener, MultiSensorManager.MultiSensorListener,
         MessageStatusListener, Contact.UpdateListener, OnGesturePerformedListener, SensorEventListener,
         LoaderManager.LoaderCallbacks<Cursor>  {
     public static final int REQUEST_CODE_ATTACH_IMAGE     = 100;
@@ -304,6 +306,8 @@ public class ComposeMessageActivity extends Activity
     private static final int LOADING_MESSAGES_AND_DRAFT_MAX_DELAY_MS = 500;
 
     private ContentResolver mContentResolver;
+
+    private MultiSensorManager mMultiSensorManager;
 
     private BackgroundQueryHandler mBackgroundQueryHandler;
 
@@ -2035,6 +2039,8 @@ public class ComposeMessageActivity extends Activity
 
         initialize(savedInstanceState, 0);
 
+        mMultiSensorManager = new MultiSensorManager((Context) ComposeMessageActivity.this, this);
+
         if (TRACE) {
             android.os.Debug.startMethodTracing("compose");
         }
@@ -2508,6 +2514,12 @@ public class ComposeMessageActivity extends Activity
                 | InputType.TYPE_TEXT_FLAG_CAP_SENTENCES
                 | InputType.TYPE_TEXT_FLAG_MULTI_LINE);
 
+        TelephonyManager tm = (TelephonyManager) getSystemService(Service.TELEPHONY_SERVICE);
+        if (MessagingPreferenceActivity.getSmartCallEnabled((Context) ComposeMessageActivity.this)
+             && tm.getCallState() == TelephonyManager.CALL_STATE_IDLE) {
+            mMultiSensorManager.enable();
+        }
+
         mIsRunning = true;
         updateThreadIdIfRunning();
         mConversation.markAsRead(true);
@@ -2540,6 +2552,11 @@ public class ComposeMessageActivity extends Activity
     } catch (Exception e) {
         Log.w("ERROR", e.toString());
     }
+
+        if (MessagingPreferenceActivity.getSmartCallEnabled((Context) ComposeMessageActivity.this)) {
+            mMultiSensorManager.disable();
+        }
+
         // remove any callback to display a progress spinner
         if (mAsyncDialog != null) {
             mAsyncDialog.clearPendingProgressDialog();
@@ -4898,6 +4915,21 @@ public class ComposeMessageActivity extends Activity
             text = mUnicodeFilter.filter(text);
         }
         return text;
+    }
+
+    @Override
+    public void onPickup() {
+        if (getRecipients().isEmpty() == false) {
+            mMultiSensorManager.disable();
+
+            // get number and attach it to an Intent.ACTION_CALL, then start
+            // the Intent
+            String number = getRecipients().get(0).getNumber();
+            Intent dialIntent = new Intent(Intent.ACTION_CALL);
+            dialIntent.setData(Uri.fromParts("tel", number, null));
+            dialIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(dialIntent);
+        }
     }
 
     @Override
